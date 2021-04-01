ce118393fa7c082c5ad27487c2fac5759006b5c9
avinashbhutekar50 <61005241+avinashbhutekar50@users.noreply.github.com>
2021-04-01 08:41:21 +0530

    Initial commit of Math.py file

---
+++ math.py
@@ -0,0 +1,15 @@
+# Add implementation
+def add(x,y):
+    return x+y
+# Subtract implementation
+def subtract(x,y):
+    return x-y
+# Multiply implementation
+def multiply(x,y):
+    return x*y
+# Divide implementation
+def divide(x,y):
+    if y=0:
+	return divide
+    else:
+	return x/y
